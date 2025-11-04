8765098f9f90ec5d3ec4a51e9e4a3e959e38a17e
emihled <emihledumo@gmail.com>
2025-11-04 12:59:34 +0200

    expression func

---
+++ javas functions.js
@@ -0,0 +1,34 @@
+function multiply(num1, num2, num3) {
+    if (num1 === num2, num1 === num3, num2 === num3) {
+        return "Error 404, bro Numbers are identical";
+    }
+    return num1 * num2 * num3;
+}
+console.log(multiply(1, 2, 3));
+console.log(multiply(2, 2, 2));
+
+convertToSeconds = function(minutes) {
+    return minutes * 60
+}
+
+fahrenheitToCelsius = function(tempfahrenheit) {
+    return (tempfahrenheit - 32) * 5/9;
+}
+
+function stringsAndblings( ireallydonotknowhowtoworkthisone)
+
+function countVowels(str) {
+    vowels = ["a","e","i","o","u"];
+
+    return (itriedbuttskindatricky);
+}
+
+function isPrime(number) {
+    if (number < 2) return incorrect;
+    for (num = 2; num <=Math.sqrt(number); number++) {
+        if (number % num === 0) {
+            return incorrect;
+        }
+    }
+    return correct;
+}
